9e708f604ade7b9a767dfd6f3d0c97007c487322
Friedrich Lindenberg <friedrich@pudo.org>
2015-12-03 13:16:00 +0100

    pagination

--- scraper.py
+++ scraper.py
@@ -10,7 +10,9 @@ db = os.environ.get('DATABASE_URI', 'sqlite:///data.sqlite')
 engine = dataset.connect(db)
 companies = engine['de_handelsregister']
 
+PAGE_SIZE = 100
 SEARCH_URL = 'https://www.handelsregister.de/rp_web/mask.do?Typ=e'
+PAGE_URL = 'https://www.handelsregister.de/rp_web/result.do?Page=%s'
 DOC_URL = 'https://www.handelsregister.de/rp_web/document.do?doctyp=UT&index=%s'
 
 STATES = ["NW", "BW", "BY", "BE", "BR", "HB", "HH", "HE",
@@ -29,7 +31,7 @@ QUERY = {
     "ort": "",
     "strasse": "",
     "suchOptionenGeloescht": "true",
-    "ergebnisseProSeite": "100",
+    "ergebnisseProSeite": PAGE_SIZE,
     "btnSuche": "Suchen"
 }
 
@@ -45,8 +47,8 @@ def scrape_state(q, state):
     failed_state = 0
     for i in count(1):
         if failed_state > 1000:
+            time.sleep(10)
             return
-        time.sleep(1.0)
         exist = list(companies.find(state=state, number=i))
         if len(exist) and len(exist) == exist[0]['result_count']:
             continue
@@ -54,47 +56,72 @@ def scrape_state(q, state):
         q["registerNummer"] = i
         sess = requests.Session()
         res = sess.post(SEARCH_URL, data=q)
-        doc = html.fromstring(res.content)
-        content = doc.find('.//div[@id="inhalt"]')
+        results = 0
+        page = 1
+        while True:
+            total, page_results = parse_results(sess, state, i, res.content)
+            results += page_results
+            if total == -1:
+                failed_state += 1
+                break
+            else:
+                failed_state = 0
+            print 'PAGE', total, page, page_results, results
+            if results >= total:
+                break
+            page += 1
+            res = sess.get(PAGE_URL % page)
 
-        count_text = content.findtext('./p')
-        if not count_text or 'Ihre Suche hat' not in count_text:
-            failed_state += 1
-            continue
-        _, count_text = count_text.split('hat', 1)
-        count_text, _ = count_text.split('Treffer', 1)
-        results = int(count_text)
-        print 'Reg %s (%s): %s' % (state, i, results)
-        failed_state = 0
 
-        current_html = ''
-        current_index = 0
-        for tr in content.findall('./table[@class="RegPortErg"]/tr'):
-            tds = tr.findall('./td')
-            if len(tds) == 1 and tds[0].get('class') == 'RegPortErg_AZ':
-                if len(current_html):
-                    current_html = '<table>%s</table>' % current_html
-                    scrape_ut(sess, state, results, i, current_html,
-                              current_index)
-                    current_index += 1
-                    current_html = ''
-            current_html += html.tostring(tr)
+def parse_results(sess, state, i, page):
+    doc = html.fromstring(page)
+    content = doc.find('.//div[@id="inhalt"]')
+
+    count_text = content.findtext('./p')
+    if not count_text or 'Ihre Suche hat' not in count_text:
+        return -1, 0
+    _, count_text = count_text.split('hat', 1)
+    count_text, _ = count_text.split('Treffer', 1)
+    results = int(count_text)
+    print 'Reg %s (%s): %s' % (state, i, results)
+
+    current_html = ''
+    current_index = 0
+    for tr in content.findall('./table[@class="RegPortErg"]/tr'):
+        tds = tr.findall('./td')
+        if len(tds) == 1 and tds[0].get('class') == 'RegPortErg_AZ':
+            if len(current_html):
+                current_html = '<table>%s</table>' % current_html
+                scrape_ut(sess, state, results, i, current_html,
+                          current_index)
+                current_index += 1
+                current_html = ''
+        current_html += html.tostring(tr)
+    return results, current_index
 
 
 def scrape_ut(sess, state, results, i, index_html, index):
-    res = sess.get(DOC_URL % index)
-    doc = html.fromstring(res.content)
-    content = doc.find('.//div[@id="inhalt"]')
-    ut_html = html.tostring(content)
-    data = {
-        'state': state,
-        'number': i,
-        'result': index,
-        'result_count': results,
-        'result_html': index_html,
-        'ut_html': ut_html
-    }
-    companies.upsert(data, ['state', 'number', 'result'])
+    if companies.find_one(state=state, number=i, result=index):
+        return
+    try:
+        res = sess.get(DOC_URL % index)
+        doc = html.fromstring(res.content)
+        content = doc.find('.//div[@id="inhalt"]')
+        if content is None:
+            return
+        ut_html = html.tostring(content)
+        data = {
+            'state': state,
+            'number': i,
+            'result': index,
+            'result_count': results,
+            'result_html': index_html,
+            'ut_html': ut_html
+        }
+        companies.upsert(data, ['state', 'number', 'result'])
+    except Exception, e:
+        print e
+        time.sleep(5)
 
 
 if __name__ == '__main__':
